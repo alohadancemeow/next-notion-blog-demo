2cf2d2a4ed8b271803aad6012b230808c5fe4f9e
Alohadancemeow <nawongsak@gmail.com>
2023-07-14 14:31:35 +0700

    👌 1st commit

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import styles from "./index.module.css";
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ posts }) {
+
+  console.log(posts, 'posts');
+  
   return (
     <div>
       <Head>
